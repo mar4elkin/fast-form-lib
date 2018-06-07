0d11783ea4278a998206adee6a3e953d85cbbe87
mar4elkin <googlcraftlol@gmail.com>
2018-06-07 23:08:18 +0300

    cleaned commits...

--- main/base.php
+++ main/base.php
@@ -3,12 +3,6 @@
   use Widget\Select as Select;
   use View\Publication as Publication;
   abstract class Base{
-/*
-    function spawn() {
-      $news = new Select();
-      return $news->from($this->table)->start();
-    }
-    abstract function show($type = 'thingz');
-    */
+
   }
 ?>
