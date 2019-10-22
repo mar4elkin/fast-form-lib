6b07c85d9c6a7aeeeee6d7ad58c08ee9f65438d9
mar4elkin <googlcraftlol@gmail.com>
2019-10-22 17:52:26 +0300

    добавления файла конфигурации для разработки

---
+++ settings.php
@@ -0,0 +1,19 @@
+<?php
+
+// settings.php 
+
+$debug_forms = [
+    'Form'      => 'TRUE',
+    'Input'     => 'TRUE',
+    'Button'    => 'TRUE',
+    'Lable'     => 'TRUE',
+    'Legend'    => 'TRUE',
+    'Fieldset'  => 'TRUE',
+    'Optgroup'  => 'TRUE',
+    'Option'    => 'TRUE',
+    'Select'    => 'TRUE',
+    'Textarea'  => 'TRUE',
+];
+
+
+
